fbf1c994f401508632449efa0b21ce69274865e1
kedz-1 <khadar7@hotmail.co.uk>
2025-11-27 16:16:56 +0000

    Parent function completed and tested

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import requests
+from src.ingestion.fetch_articles import fetch_article
+from src.publishers.send_to_sqs import send_sqs
+from dotenv import load_dotenv
+import os
+import logging
+
+load_dotenv()
+guardian_key = os.getenv('GUARDIAN_KEY')
+
+logging.basicConfig(level = logging.INFO)
+
+def fetch_and_send_articles(search_term, reference, date_from=None):
+
+    try: 
+
+        filtered_articles = fetch_article(guardian_key, search_term, date_from)
+        logging.info('Filtered article function has worked')
+
+        response = send_sqs(reference, filtered_articles)
+
+        logging.info('send_sqs function has worked')
+
+    except Exception as e:
+        logging.error(f'Error has occured: {e}')
+
+
+    return response
+
+#fetch_and_send_articles('Trump', '2022-01-01', 'guardian_content')
+
+
+#print(fetch_and_send_articles('Trump', '2022-01-01', 'guardian_content'))
